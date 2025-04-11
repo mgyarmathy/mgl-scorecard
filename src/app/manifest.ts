f796651a0e9ccf95c1481abe9f01b1ebd816c0f3
Michael Gyarmathy <mgyarmathy@pgahq.com>
2025-04-11 15:48:29 -0500

    add app icon and update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,18 +7,18 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "MGL Scorecard",
     short_name: "MGL Scorecard",
     description: "A scorecard app for MGL",
-    start_url: "/",
+    start_url: "/mgl-scorecard",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/mgl-scorecard/MGL_app_icon_192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/mgl-scorecard/MGL_app_icon_512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
