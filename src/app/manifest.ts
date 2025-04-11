ef1a51021757e9ef7cc3c9a2741572313e2a42c2
Michael Gyarmathy <mgyarmathy@pgahq.com>
2025-04-11 15:57:39 -0500

    update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,18 +7,18 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "MGL Scorecard",
     short_name: "MGL Scorecard",
     description: "A scorecard app for MGL",
-    start_url: "/mgl-scorecard",
+    start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
     icons: [
       {
-        src: "/mgl-scorecard/MGL_app_icon_192x192.png",
+        src: "/MGL_app_icon_192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/mgl-scorecard/MGL_app_icon_512x512.png",
+        src: "/MGL_app_icon_512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
